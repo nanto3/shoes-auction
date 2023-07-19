7ab96b920db5528eea5087d05f095e33adc58297
seungwoo <jsu7375@gmail.com>
2023-07-20 00:09:58 +0900

    refactor: UserUtil 작성
    isEmail, hash 관련 함수들 UserUtil class로 이동

--- src/models/UserUtil.ts
+++ src/models/UserUtil.ts
@@ -3,15 +3,15 @@ import envConfig from '../configs/env.config';
 import { EMAIL_REGEX } from "../constants/const";
 
 export default class UserUtil {
-  static isEmail( value ) {
+  static isEmail( value: string ): boolean {
     return EMAIL_REGEX.test( value );
   }
 
-  static async hashPassword( password: string ) { 
+  static async hashPassword( password: string ): Promise<string> { 
     return await bcrypt.hash( password, await bcrypt.genSalt( +envConfig.passwordSalt ) );
   }
 
-  static async validatePassword( password: string, dbPassword: string ) {
+  static async validatePassword( password: string, dbPassword: string ): Promise<boolean> {
     return await bcrypt.compare( password, dbPassword );
   }
 }
