18fd1a47fc22269cf178ce989a0a2a394bfd5710
seungwoo <jsu7375@gmail.com>
2023-07-20 00:38:41 +0900

    style: handlerError와 emitNotFoundError 위치 변경

--- src/utils/responder.ts
+++ src/utils/responder.ts
@@ -20,11 +20,11 @@ interface ResError {
 const respondError = ( res: Response, error: ResError ) => 
   res.status( error.status || 500 ).json({ message: error.message || 'not defined error' });
 
-export const emitNotFoundError = ( req: Request, res: Response, next: NextFunction ) => 
-  next( new ResException( 404, 'not found' ) );
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handleError = ( error: ResException | Error, req: Request, res: Response, next: NextFunction ) => {
   console.log( error ); 
   respondError( res, error ) ;
 };
+
+export const emitNotFoundError = ( req: Request, res: Response, next: NextFunction ) => 
+  next( new ResException( 404, 'not found' ) );
