2fcda436b743a1a8434ba54e90dcce11fd99c8ff
seungwoo <jsu7375@gmail.com>
2023-07-23 12:36:16 +0900

    style: User.init 각 속성 comment 제거

--- src/entities/UserEntity.ts
+++ src/entities/UserEntity.ts
@@ -20,18 +20,15 @@ export const UserFactory = ( sequelize: Sequelize ) => User.init({
     allowNull: false,
   },    
   uuid: {
-    comment: '유저 uuid',
     type: DataTypes.UUID,
     defaultValue: DataTypes.UUIDV4,
     primaryKey: true,
   },
   email: {
-    comment: '유저 mail',
     type: DataTypes.STRING,
     allowNull: false,
   },
   password: {
-    comment: '비밀번호',
     type: DataTypes.STRING,
     allowNull: false,
   },
