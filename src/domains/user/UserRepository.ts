1313e5d7d9a59c900856561922da0d25a3c37ad8
seungwoo <jsu7375@gmail.com>
2023-07-27 22:41:35 +0900

    feat: createUser 매개변수 transaction 추가

--- src/domains/user/UserRepository.ts
+++ src/domains/user/UserRepository.ts
@@ -2,8 +2,8 @@ import { Transaction, Attributes, CreationAttributes } from 'sequelize';
 import User from '../../entities/UserEntity';
 
 export default class UserRepository {
-  async createUser( user: CreationAttributes<User> ) {
-    return await User.create( user );
+  async createUser( user: CreationAttributes<User>, transaction?: Transaction ) {
+    return await User.create( user, { transaction });
   }
 
   async findOneBy<T extends keyof Attributes<User>>( where: Record<T, unknown>, transaction?: Transaction ) {
