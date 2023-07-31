6cad4bd85a94fbcabb54bd54a37697c6372c97b3
seungwoo <jsu7375@gmail.com>
2023-08-01 00:36:23 +0900

    refactor: setReceiveOption 타입들 제거

--- src/setReceiveOptions.ts
+++ src/setReceiveOptions.ts
@@ -1,35 +1,12 @@
-import { Express, Request, Response, NextFunction, json, urlencoded } from 'express';
-import { Options, OptionsText, OptionsJson, OptionsUrlencoded } from 'body-parser';
-import cors, { CorsOptions } from 'cors';
+import { Express, json, urlencoded } from 'express';
+import cors from 'cors';
 
-
-type ReceiveMethod<T> = ( options: T ) => ( req: Request, res: Response, next: NextFunction ) => void;
-interface ReceiveTool {
-  raw?: ReceiveMethod<Options>;
-  text?: ReceiveMethod<OptionsText>;
-  json?: ReceiveMethod<OptionsJson>;
-  urlencoded?: ReceiveMethod<OptionsUrlencoded>;
-  cors?: ReceiveMethod<CorsOptions>;
-}
-
-interface ReceiveConfig {
-  raw?: Options;
-  text?: OptionsText;
-  json?: OptionsJson;
-  urlencoded?: OptionsUrlencoded;
-  cors?: CorsOptions;
-}
-const receiveConfig: ReceiveConfig = { 
-  json: { limit: '50mb' },
-  urlencoded: {
+export const setReceiveOptions = ( app: Express ) => {
+  app.use( json({ limit: '50mb' }) );
+  app.use( urlencoded({
     limit: '50mb',
     extended: false,
     parameterLimit: 1_000_000,
-  },
-};
-
-export const setReceiveOptions = ( app: Express ) => {
-  app.use( json( receiveConfig.json ) );
-  app.use( urlencoded( receiveConfig.urlencoded ) );
+  }) );
   app.use( cors({ credentials: true }) );
 };
