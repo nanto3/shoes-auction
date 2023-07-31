0a67bd21d07e896b730d2361aab69ee2c39bf60e
seungwoo <jsu7375@gmail.com>
2023-08-01 00:46:05 +0900

    refactor: errorHandler 위치 app -> router 이동

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,13 @@
 import express from 'express';
 import envConfig from './configs/envConfig';
-import routers from './domains/routers';
+import router from './domains/router';
 import { setReceiveOptions } from './setReceiveOptions';
-import { emitNotFoundError, handleError } from './utils/responder';
 import { checkDbConnection } from './entities';
 
 const app: express.Express = express();
 
 setReceiveOptions( app );
-
-app.use( routers );
-app.use( emitNotFoundError );
-app.use( handleError );
+app.use( router );
 
 checkDbConnection.then( () => 
   app.listen( envConfig.port , async () => 
--- src/domains/router.ts
+++ src/domains/router.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import UserRouter from './user/UserController';
 import ProductRouter from './product/ProductController';
 import AuctionRouter from './auction/AuctionController';
+import { emitNotFoundError, handleError } from '../utils/responder';
 
 const router = Router();
 
 router.use( '/users', UserRouter );
 router.use( '/products', ProductRouter );
 router.use( '/auctions', AuctionRouter );
+
+router.use( emitNotFoundError );
+router.use( handleError );
 
 export default router;
