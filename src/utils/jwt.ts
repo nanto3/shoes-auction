d5f7b08f359c6e620b20f5d964135947a5930b03
seungwoo <jsu7375@gmail.com>
2023-07-31 22:31:47 +0900

    refactor: jwt verify 에러 메세지 체크 수정
    string 리터럴로 에러체크 하던 것 상수로 변경

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,6 +5,9 @@ import envConfig from '../configs/envConfig';
 const jwtSecret = envConfig.jwtSecret;
 
 const HS256 = 'HS256';
+const INVALID_SIGNATURE = 'invalid signature';
+const JWT_EXPIRED = 'jwt expired';
+
 const A_DAY_BY_SECONDS = 60 * 60 * 24;
 export const EXPIRY_OF_ACCESS_TOKEN_BY_SECOND = A_DAY_BY_SECONDS * 7;
 export const EXPIRY_OF_REFRESH_TOKEN_BY_SECOND = A_DAY_BY_SECONDS * 30;
@@ -30,10 +33,10 @@ export const verify = ( token: string ): JwtPayload => {
   try {
     return jwt.verify( token, jwtSecret ) as JwtPayload;
   } catch ( error ) {
-    if ( error.message === 'invalid signature' ) {
+    if ( error.message === INVALID_SIGNATURE ) {
       throw new ResException( 401, 'not authenticated' );
     }
-    if ( error.message === 'jwt expired' ) {
+    if ( error.message === JWT_EXPIRED ) {
       throw new ResException( 401, 'login expired' );
     }
   }
