09cfa3c7b50d3f70aafe73ca6c24a45363eedf25
seungwoo <jsu7375@gmail.com>
2023-08-04 15:53:30 +0900

    test: join test 작성

--- src/tests/user.service.test.ts
+++ src/tests/user.service.test.ts
@@ -2,49 +2,48 @@ import { describe, it, test, expect } from "@jest/globals";
 import UserService from "../domains/user/user.service";
 import ResException, { excptIfTruthy, excptIfFalsy } from "../utils/ResException";
 
-describe( 'excptIfFormat', () => {
-  it( 'should throw error if arguement truthy', () => {
-    expect( 1 ).toBe( 2 );
-    // try {
-    //   // const aaa = excptIfTruthy( false );
-    //   const a = 1;
-    // } catch ( error ) {
-    //   expect( error ).toEqual( new ResException( 400 ) );
-    // }
+describe( 'user-service', () => {
+  const UserRepository = () => {
+    const users = [];
+    return {
+      createUser: ( user ) => {
+        const body = { ...user, id: users.length + 1 };
+        users.push( body );
+        return body;
+      },
+      findOneBy: ({ email }: any ) => users.find( anUser => anUser.email === email ),
+    };
+  };
+
+  describe( 'join', () => {
+    const email = 'abc123@test.com';
+    const password = '1234';
+
+    it( 'returns user with id', async () => {
+      const userService = new UserService( UserRepository() );
+
+      const user = await userService.join( email, password );
+    
+      expect( user ).toEqual({ id: 1, email, password });
+    });
+
+    it( 'throws error - already registered email', async () => {
+      const userService = new UserService( UserRepository() );
+
+      expect.assertions( 1 );
+      
+      try {
+        await userService.join( email, password );
+        await userService.join( email, password );
+      } catch ( error ) {
+        expect( error.message ).toMatch( 'already registered email' );
+      }
+    });
   });
-  // it( 'should throw error if arguement falsy', () => {
-  //   try {
-  //     const aaa = excptIfFalsy( true );
-  //     expect( aaa ).toEqual( new ResException( 400 ) );
-  //   } catch ( error ) {
-  //     expect( error ).toEqual( new ResException( 400 ) );
-  //   }
+
+  // describe( 'login', () => {
+  //   it( 'returns two jwt with userUuid', () => {
+      
+  //   });
   // });
 });
-
-// describe( 'user-service', () => {
-//   test( 'join', async () => {
-//     const users = [];
-//     const userRepository = {
-//       createUser: ( user ) => {
-//         const body = { ...user, id: 1 };
-//         users.push( body );
-//         return body;
-//       },
-//       findByEmail: ( email ) => users.find( anUser => anUser.email === email ),
-//     };
-
-//     const email = 'abc123@naver.com';
-//     // const email = 'abc123naver.com';
-//     const password = '1234';
-//     const userService = new UserService( userRepository );
-    
-//     try {
-//       expect( await userService.join( new User({ email, password }) ) )
-//         .toEqual({ id: 1, email, password });
-//     } catch ( error ) {
-//       expect( error )
-//         .toEqual( new ResException( 400, 'wrong email format' ) );
-//     }
-//   }); 
-// });
