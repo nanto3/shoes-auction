f7da1446d6684aff6c83eef16c6af11401a85457
seungwoo <jsu7375@gmail.com>
2023-08-08 23:35:47 +0900

    test: join 테스트에서 에러 테스트를 먼저 실행

--- src/tests/user.service.test.ts
+++ src/tests/user.service.test.ts
@@ -1,79 +1,82 @@
 import { describe, it, test, expect } from "@jest/globals";
 import UserService from "../domains/user/user.service";
 
-describe( 'join', () => {
-  const UserRepository = () => {
-    const users = [];
-    return {
-      createUser: ( user ) => {
-        const body = { ...user, id: users.length + 1 };
-        users.push( body );
-        return body;
-      },
-      findOneBy: ({ email }: any ) => users.find( anUser => anUser.email === email ),
+describe( 'user-service', () => {
+
+  describe( 'join', () => {
+    const UserRepository = () => {
+      const users = [];
+      return {
+        createUser: ( user ) => {
+          const body = { ...user, id: users.length + 1 };
+          users.push( body );
+          return body;
+        },
+        findOneBy: ({ email }: any ) => users.find( anUser => anUser.email === email ),
+      };
     };
-  };
-  const email = 'abc123@test.com';
-  const password = '1234';
-
-  it( 'returns user with id', async () => {
-    const userService = new UserService( UserRepository() );
-
-    const user = await userService.join( email, password );
+    const email = 'abc123@test.com';
+    const password = '1234';
+  
+    it( 'throws error - already registered email', async () => {
+      const userService = new UserService( UserRepository() );
+  
+      try {
+        await userService.join( email, password );
+        await userService.join( email, password );
+  
+      } catch ( error ) {
+        expect( error.message ).toMatch( 'already registered' );
+      }
+    });
     
-    expect( user ).toEqual({ id: 1, email, password });
+    it( 'returns user with id', async () => {
+      const userService = new UserService( UserRepository() );
+  
+      const user = await userService.join( email, password );
+      
+      expect( user ).toEqual({ id: 1, email, password });
+    });
   });
-
-  it( 'throws error - already registered email', async () => {
-    const userService = new UserService( UserRepository() );
-
-    try {
-      await userService.join( email, password );
-      await userService.join( email, password );
-
-    } catch ( error ) {
-      expect( error.message ).toMatch( 'already registered' );
-    }
-  });
-});
-
-describe( 'login', () => {
-  const firstUserMail = 'abc@test.com';
-  const firstUserPassword = '1234';
-  const UserRepository = () => {
-    const users = [ { 
-      id: 1, 
-      email: firstUserMail, 
-      password: firstUserPassword, 
-      validatePassword: ( _password ) => users[0].password === _password, 
-    } ];
-    return {
-      createUser: ( () => ({}) ) as any,
-      findOneBy: ({ email }: any ): any => users.find( user => user.email === email ), 
+  
+  describe( 'login', () => {
+    const firstUserMail = 'abc@test.com';
+    const firstUserPassword = '1234';
+    const UserRepository = () => {
+      const users = [ { 
+        id: 1, 
+        email: firstUserMail, 
+        password: firstUserPassword, 
+        validatePassword: ( _password ) => users[0].password === _password, 
+      } ];
+      return {
+        createUser: ( () => ({}) ) as any,
+        findOneBy: ({ email }: any ): any => users.find( user => user.email === email ), 
+      };
     };
-  };
-
-  it( 'throws error - not registered user', async () => {
-    const email =  'a' + firstUserMail;
-    const password = firstUserPassword;
-    const userService = new UserService( UserRepository() );
-      
-    try {
-      await userService.login( email, password );
-
-    } catch ( error ) {
-      expect( error.message ).toMatch( 'not registered' );
-    }
-  });
-
-  it( 'throws error - wrong password', async () => {
-    const userService = new UserService( UserRepository() );
-      
-    try {
-      await userService.login( firstUserMail, firstUserPassword + 'a' );
-
-    } catch ( error ) {
-      expect( error.message ).toMatch( 'wrong password' );
-    }
+  
+    it( 'throws error - not registered user', async () => {
+      const email =  'a' + firstUserMail;
+      const password = firstUserPassword;
+      const userService = new UserService( UserRepository() );
+        
+      try {
+        await userService.login( email, password );
+  
+      } catch ( error ) {
+        expect( error.message ).toMatch( 'not registered' );
+      }
+    });
+  
+    it( 'throws error - wrong password', async () => {
+      const userService = new UserService( UserRepository() );
+        
+      try {
+        await userService.login( firstUserMail, firstUserPassword + 'a' );
+  
+      } catch ( error ) {
+        expect( error.message ).toMatch( 'wrong password' );
+      }
+    });
   });
 });
