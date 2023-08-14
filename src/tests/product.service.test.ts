b77cfdf093f95c5db6d33ad9010331670c7667e5
seungwoo <jsu7375@gmail.com>
2023-08-15 00:08:42 +0900

    test: createProduct 작성

---
+++ src/tests/product.service.test.ts
@@ -0,0 +1,34 @@
+import { describe, it, test, expect } from "@jest/globals";
+import { ProductService } from "../domains/product/product.service";
+
+describe( 'product-service', () => {
+
+  describe( 'join', () => {
+    const ProductRepository = () => {
+      const products = [];
+      return {
+        createProduct: ( product ) => {
+          const body = { ...product, id: products.length + 1 };
+          products.push( body );
+          return body;
+        },
+        findOneBy: ({ email }: any ) => products.find( anUser => anUser.email === email ),
+      };
+    };
+    const productVO = {
+      userId: 1,
+      brand: 'NIKE' as 'NIKE'|'ADIDAS'|'ETC',
+      name: 'jordan1 cichago',
+      price: 500_000,
+      auctionCloseDate: new Date( '2023-08-20 22:00:00' ),
+    };
+    
+    it( 'returns product with id', async () => {
+      const productService = new ProductService( ProductRepository() );
+  
+      const product = await productService.createProdudct( productVO );
+      
+      expect( product ).toEqual({ id: 1, ...productVO });
+    });
+  });
+});
