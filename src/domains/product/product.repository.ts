c373338eae9d61d8f37e51aca0d9f97aec801362
seungwoo <jsu7375@gmail.com>
2023-08-14 23:30:17 +0900

    feat: getProductById 작성

--- src/domains/product/product.repository.ts
+++ src/domains/product/product.repository.ts
@@ -1,3 +1,10 @@
+import { Transaction, Attributes, CreationAttributes } from 'sequelize';
+import Product from "../../entities/product.entity";
+import ErrorException from '../../utils/ErrorException';
+
 export class ProductRepository {
   
+  async findOneBy<T extends keyof Attributes<Product>>( where: Record<T, Product[T]>, transaction?: Transaction ) {
+    return await Product.findOne({ where, transaction });
+  }
 }
--- src/domains/product/product.service.ts
+++ src/domains/product/product.service.ts
@@ -1,3 +1,9 @@
+import { type ProductRepository } from './product.repository';
+
 export class ProductService {
-  
+  constructor( private productRepository: ProductRepository ) {}
+
+  async getProductById( id: number ) {
+    return await this.productRepository.findOneBy({ id });
+  }
 }
