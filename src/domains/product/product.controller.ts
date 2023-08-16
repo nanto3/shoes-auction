620de963b4fa165beccc24624418c81f1d600756
seungwoo <jsu7375@gmail.com>
2023-08-17 00:30:15 +0900

    style: 상품_조회 -> 상품_목록_조회 productController 메소드명 변경

--- src/domains/product/product.controller.ts
+++ src/domains/product/product.controller.ts
@@ -5,7 +5,17 @@ import { excptIfNotType, excptIfFalsy } from "../../utils/ErrorException";
 export class ProductController {
   constructor( private productService: ProductService ) {}
 
-  상품_조회 = Get( '' )
+  상품_등록 = Post( '' )
+  ( async ({ body }) => {
+    const { userId, brand, name, price, image, auctionCloseDate } = body;
+    excptIfNotType( 'string', name, image, auctionCloseDate );
+    excptIfNotType( 'number', userId, price );
+    excptIfFalsy([ 'NIKE','ADIDAS','ETC' ].some( elem => elem === brand ) );
+
+    return { product: this.productService.createProdudct({ userId, brand, name, price, image, auctionCloseDate }) };
+  });
+
+  상품_목록_조회 = Get( '' )
   ( async ({ query }) => {
     const { page, limit, brand } = query;
     excptIfFalsy([ 'NIKE','ADIDAS','ETC' ].some( elem => elem === brand ) );
@@ -15,15 +25,5 @@ export class ProductController {
       limit: +limit,
       brand: brand as 'NIKE'|'ADIDAS'|'ETC',
     });
-  });
-
-  상품_등록 = Post( '' )
-  ( async ({ body }) => {
-    const { userId, brand, name, price, image, auctionCloseDate } = body;
-    excptIfNotType( 'string', name, image, auctionCloseDate );
-    excptIfNotType( 'number', userId, price );
-    excptIfFalsy([ 'NIKE','ADIDAS','ETC' ].some( elem => elem === brand ) );
-
-    return { product: this.productService.createProdudct({ userId, brand, name, price, image, auctionCloseDate }) };
   });
 }
