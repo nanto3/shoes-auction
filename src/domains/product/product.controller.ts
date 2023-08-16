cdbbf14be98fb5c19b03db6b4a4558b849ee0eb1
seungwoo <jsu7375@gmail.com>
2023-08-17 00:15:53 +0900

    feat: 상품 조회, 등록 brand validation 추가

--- src/domains/product/product.controller.ts
+++ src/domains/product/product.controller.ts
@@ -1,6 +1,6 @@
 import { type ProductService } from "./product.service";
 import { Get, Post, Patch } from "../../utils/frame-util/3-layer-helper";
-import { excptIfNotType } from "../../utils/ErrorException";
+import { excptIfNotType, excptIfFalsy } from "../../utils/ErrorException";
 
 export class ProductController {
   constructor( private productService: ProductService ) {}
@@ -8,6 +8,7 @@ export class ProductController {
   상품_조회 = Get( '' )
   ( async ({ query }) => {
     const { page, limit, brand } = query;
+    excptIfFalsy([ 'NIKE','ADIDAS','ETC' ].some( elem => elem === brand ) );
 
     return await this.productService.getProducts({ 
       page: +page, 
@@ -19,8 +20,9 @@ export class ProductController {
   상품_등록 = Post( '' )
   ( async ({ body }) => {
     const { userId, brand, name, price, image, auctionCloseDate } = body;
-    excptIfNotType( 'string', brand, name, image, auctionCloseDate );
+    excptIfNotType( 'string', name, image, auctionCloseDate );
     excptIfNotType( 'number', userId, price );
+    excptIfFalsy([ 'NIKE','ADIDAS','ETC' ].some( elem => elem === brand ) );
 
     return { product: this.productService.createProdudct({ userId, brand, name, price, image, auctionCloseDate }) };
   });
